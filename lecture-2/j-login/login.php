a43a168d1df44abeec0f996fbf98eeba01f1a4d8
Jeremiah Wodke <jeremiah@Jeremiahs-iMac.local>
2017-04-27 12:59:48 -0600

    onto mvc for lecture-2

--- lecture-2/j-login/login.php
+++ lecture-2/j-login/login.php
@@ -7,7 +7,11 @@
   if (isset($_POST['username']) && isset($_POST['password'])) {
     if ($_POST['username'] == USER && $_POST['password'] == PASS) {
       $_SESSION['authenticated'] = true;
-      header('Location: home.php');
+      // redirect user to home page, using absolute path, per
+      // http://us2.php.net/manual/en/function.header.php
+      $host = $_SERVER["HTTP_HOST"];
+      $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+      header('Location: http://'.$host.$path.'/home.php');
     } else {
       echo '<h2 style="color: red;">Wrong Username or Password</h2>';
     }
@@ -18,6 +22,6 @@
 
 <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
   <input type="text" name="username">
-  <input type="text" name="password">
+  <input type="password" name="password">
   <input type="submit">
 </form>
